6d983584202f41d2e96eed96611aee7ab4114368
akira-ccg <sdimitrij3@gmail.com>
2026-03-28 13:59:45 +0300

    feat[frontend]: Login page done

--- frontend/src/pages/LoginPage/Login.tsx
+++ frontend/src/pages/LoginPage/Login.tsx
@@ -1,26 +1,29 @@
 import "./Login.css"
-
+import st from "../../shared/assets/images/svg/pngwing.com 1.svg";
+ 
 function Login(){
     return (
     <main className="main">
         <form className="login-card">
         <h1>Login</h1>
-        <label>
-            Email
-            <input type="email" placeholder="Enter your email"/>
-        </label>
+        <div className="form-group">
+    <label>Email or username</label>
+    <input type="email" placeholder="Enter your email" />
+  </div>
 
-        <label>
-            Password
-            <input type="password" placeholder="Enter your password"/>
-        </label>
+  <div className="form-group">
+    <label>Password</label>
+    <input type="password" placeholder="Enter your password" />
+  </div>
 
-        <button type="submit">Login</button>
-        <a>Forgot password?</a>
-
+    <button className="next-btn" type="submit">
+    <img src={st} alt="next"/>
+    </button>
+  <a href="#">Forgot password?</a>
         </form>
     </main>
   )
 }
 
 export default Login
+
