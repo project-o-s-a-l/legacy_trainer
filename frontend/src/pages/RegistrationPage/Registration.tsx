071fd6350c3cc1b28020db1e9ad1dfef0b0a3b73
MaSidrenko <nexon863@gmail.com>
2026-04-02 22:41:52 +0300

    feat[frontend]: global css styles moved from Registration.css

--- frontend/src/pages/RegistrationPage/Registration.tsx
+++ frontend/src/pages/RegistrationPage/Registration.tsx
@@ -3,31 +3,31 @@ import "./Registration.css"
 function Registration(){
 
     return (
-    <div className="registration-page">
-      <div className="registration-card">
+    <div className="registration-page flex-center">
+      <div className="registration-card card card-code-gradient ">
         <h1 className="registration-title">Registration</h1>
 
-        <form className="registration-form">
+        <form className="registration-form flex-col">
           <label className="form-label">
             Username
-            <input type="text" placeholder="Enter username" className="form-input" />
+            <input type="text" placeholder="Enter username" className="input form-input" />
           </label>
 
           <label className="form-label">
             Email or username
-            <input type="text" placeholder="Enter email or username" className="form-input" />
+            <input type="text" placeholder="Enter email or username" className="input form-input" />
           </label>
           <label className="form-label">
             Password
-            <input type="password" placeholder="Enter password" className="form-input" />
+            <input type="password" placeholder="Enter password" className="input form-input" />
           </label>
 
           <label className="form-label">
             Confirm your password
-            <input type="password" placeholder="Enter your password" className="form-input" />
+            <input type="password" placeholder="Enter your password" className="input form-input" />
           </label>
 
-          <button type="submit" className="btn-submit-registration">
+          <button type="submit" className="btn-ghost btn-submit-registration">
             Get code
           </button>
         </form>
