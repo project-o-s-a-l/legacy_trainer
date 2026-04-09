5d7fb4449ff2475dadfcc3177e65956217e71b1d
MaSidrenko <nexon863@gmail.com>
2026-04-09 11:37:04 +0300

    feat[frontend]: change png for home page

--- frontend/src/pages/HomePage/Home.tsx
+++ frontend/src/pages/HomePage/Home.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 		<div>
 			<div className="about-site">
 				<div className="about-site-content flex-between-start">
-					<div className="text-main about-site-text-container">
+					<div className="text-main about-site-text-container box-light">
 						<span className="about-site-span">
 							<h2 className="about-site-h2">
 								A little about the site
@@ -25,20 +25,22 @@ export default function Home() {
 					<img className="img-pc-main" src={pc} alt="" />
 				</div>
 			</div>
-			<div className="flex-between-center">
+			<div className="flex-between-center padding-bottom-main">
 				<img className="first-bug-main" src={zeroGlith} alt="" />
-				<div className="text-main main-page-login-text-container text-center flex-col-center">
-					<p>Ready to test your code?</p>
-					<span>
-						Check the code. <br /> Get advice.
-					</span>
-					<span>
-						Upload your code, select the language and difficulty
-						level, and get instant feedback and recommendations for
-						improvement. Learn by doing and improve your programming
-						skills with us.
-					</span>
-					<div className="btn-main-page-login-container flex-col-center">
+				<div className="text-main main-page-login-text-container text-center flex-col-center box-light">
+					<div className="">
+						<p>Ready to test your code?</p>
+						<span>
+							Check the code. <br /> Get advice.
+						</span>
+						<span>
+							Upload your code, select the language and difficulty
+							level, and get instant feedback and recommendations
+							for improvement. Learn by doing and improve your
+							programming skills with us.
+						</span>
+					</div>
+					<div className="btn-main-page-login-container">
 						<Button>Registration</Button>
 						<Button className="btn-main-page-sign-in">
 							Sign In
