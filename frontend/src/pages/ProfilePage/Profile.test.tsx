620f29e115cdf8922fdca23b2478dd21c6618de9
MaSidrenko <nexon863@gmail.com>
2026-04-21 10:07:39 +0300

    tests[frontend]: tests for profile page

---
+++ frontend/src/pages/ProfilePage/Profile.test.tsx
@@ -0,0 +1,176 @@
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import { MemoryRouter, replace } from "react-router-dom";
+import { vi, describe, it, expect, beforeEach, afterEach } from "vitest";
+import Profile from "./Profile";
+import { fetchProfileInfo } from "@/features/getProfileInfo/getProfileInfo";
+
+const mockNavigate = vi.fn();
+
+vi.mock("react-router-dom", async () => {
+	const actual =
+		await vi.importActual<typeof import("react-router-dom")>(
+			"react-router-dom",
+		);
+
+	return {
+		...actual,
+		useNavigate: () => mockNavigate,
+	};
+});
+
+vi.mock("@/features/getProfileInfo/getProfileInfo", () => ({
+	fetchProfileInfo: vi.fn(),
+}));
+
+const mockedFetchProfileInfo = vi.mocked(fetchProfileInfo);
+
+const profileMock = {
+	username: "whitefox",
+	email: "whitefox@exapmle.com",
+	memberSince: "2024-01-10",
+	lastSeen: "2026-04-20",
+	isOnline: true,
+	avatarUrl: "/avatar.png",
+	points: 1500,
+	tasksCompleted: {
+		easy: 10,
+		medium: 5,
+		hard: 2,
+	},
+	averageGrade: {
+		easy: 95,
+		medium: 88,
+		hard: 80,
+	},
+};
+
+function renderProfile() {
+	return render(
+		<MemoryRouter>
+			<Profile />
+		</MemoryRouter>,
+	);
+}
+
+describe("Profile", () => {
+	beforeEach(() => {
+		vi.clearAllMocks();
+
+		vi.spyOn(URL, "createObjectURL").mockReturnValue("blob:test-preview");
+		vi.spyOn(URL, "revokeObjectURL").mockImplementation(() => {});
+		vi.spyOn(window, "alert").mockImplementation(() => {});
+	});
+
+	afterEach(() => {
+		vi.resetAllMocks();
+	});
+
+	it("render loading state", () => {
+		mockedFetchProfileInfo.mockReturnValue(new Promise(() => {}));
+
+		renderProfile();
+
+		expect(screen.getByText("Loading...")).toBeInTheDocument();
+	});
+
+	it("render success profile", async () => {
+		mockedFetchProfileInfo.mockResolvedValue(profileMock);
+
+		renderProfile();
+
+		expect(
+			await screen.findByText(profileMock.username),
+		).toBeInTheDocument();
+		expect(screen.getByText(profileMock.email)).toBeInTheDocument();
+		expect(screen.getByText(profileMock.memberSince)).toBeInTheDocument();
+		expect(screen.getByText(profileMock.lastSeen)).toBeInTheDocument();
+		expect(screen.getByText("online")).toBeInTheDocument();
+		expect(
+			screen.getByText("Points: " + profileMock.points.toString()),
+		).toBeInTheDocument();
+
+		const img = screen.getByAltText("Profile Image") as HTMLImageElement;
+		expect(img.src).toContain(profileMock.avatarUrl);
+	});
+
+	it("redirect on /login if unauthorized", async () => {
+		mockedFetchProfileInfo.mockRejectedValue(new Error("Unauthorized"));
+
+		renderProfile();
+		await waitFor(() => {
+			expect(mockNavigate).toHaveBeenCalledWith("/login", {
+				replace: true,
+			});
+		});
+	});
+
+	it("Show fallback error message if fetch fails", async () => {
+		mockedFetchProfileInfo.mockRejectedValue(new Error("Server exploded"));
+
+		renderProfile();
+
+		expect(
+			await screen.findByText("Failed to load profile information"),
+		).toBeInTheDocument();
+	});
+
+	it("Change avatar preview on file select", async () => {
+		mockedFetchProfileInfo.mockResolvedValue(profileMock);
+
+		renderProfile();
+		await screen.findByText("whitefox");
+
+		const input = screen.getByTestId("avatar-input") as HTMLInputElement;
+
+		const file = new File(["avatar"], "avatar.png", { type: "image/png" });
+
+		fireEvent.change(input, {
+			target: {
+				files: [file],
+			},
+		});
+
+		const avatar = screen.getByAltText("Profile Image");
+		expect(URL.createObjectURL).toHaveBeenCalledWith(file);
+		expect(avatar).toHaveAttribute("src", "blob:test-preview");
+	});
+
+	it("Show alert on invalid file type", async () => {
+		mockedFetchProfileInfo.mockResolvedValue(profileMock);
+
+		renderProfile();
+		await screen.findByText("whitefox");
+
+		const input = screen.getByTestId("avatar-input") as HTMLInputElement;
+		const badFile = new File(["text"], "notes.txt", { type: "text/plain" });
+
+		fireEvent.change(input, {
+			target: {
+				files: [badFile],
+			},
+		});
+
+		expect(window.alert).toHaveBeenCalledWith(
+			"Пожалуйста, выберите файл в формате PNG или JPG",
+		);
+	});
+
+	it("Free blod URL on unmount", async () => {
+		mockedFetchProfileInfo.mockResolvedValue(profileMock);
+
+		const { unmount } = renderProfile();
+		await screen.findByText("whitefox");
+		const input = screen.getByTestId("avatar-input") as HTMLInputElement;
+		const file = new File(["avatar"], "avatar.png", { type: "image/png" });
+
+		fireEvent.change(input, {
+			target: {
+				files: [file],
+			},
+		});
+
+		unmount();
+
+		expect(URL.revokeObjectURL).toHaveBeenCalledWith("blob:test-preview");
+	});
+});
