94302fed4485dcb5aeeabb643471e633aa13d16d
MaSidrenko <nexon863@gmail.com>
2026-06-09 12:43:08 +0300

    feat[frontend]: delete a unused pages

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,8 +1,4 @@
-export { About } from "./AboutPage/index.ts";
-export { Contact } from "./ContactPage/index.ts";
 export { Home } from "./HomePage/index.ts";
-export { Possibilites } from "./PossibilitesPage/index.ts";
-export { Support } from "./SupportPage/index.ts";
 export { Login } from "./LoginPage/index.ts";
 export { CodeEditor } from "./CodeBlockPage/index.ts";
 export { Registration } from "./RegistrationPage/index.ts";
